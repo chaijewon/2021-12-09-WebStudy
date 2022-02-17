38179661a9432f32ad380274b362fae6fe80ab7e
user <user@DESKTOP-COJ9858>
2022-02-17 13:26:51 +0900

    1111

--- DataBoardProject/src/main/java/com/sist/dao/DataBoardDAO.java
+++ DataBoardProject/src/main/java/com/sist/dao/DataBoardDAO.java
@@ -35,9 +35,9 @@ public class DataBoardDAO {
     	try
     	{
     		getConnection();
-    		String sql="SELECT no,subject,name,regdate,hit,num "
-    				  +"FROM (SELECT no,subject,name,regdate,hit,rownum as num "
-    				  +"FROM (SELECT no,subject,name,regdate,hit "
+    		String sql="SELECT no,subject,name,regdate,hit,group_tab,num "
+    				  +"FROM (SELECT no,subject,name,regdate,hit,group_tab,rownum as num "
+    				  +"FROM (SELECT no,subject,name,regdate,hit,group_tab "
     				  +"FROM databoard ORDER BY group_id DESC, group_step ASC)) "
     				  +"WHERE num BETWEEN ? AND ?";
     		int rowSize=10;
@@ -57,6 +57,7 @@ public class DataBoardDAO {
     			vo.setName(rs.getString(3));
     			vo.setRegdate(rs.getDate(4));
     			vo.setHit(rs.getInt(5));
+    			vo.setGroup_tab(rs.getInt(6));;
     			list.add(vo);
     		}
     		rs.close();
@@ -71,6 +72,137 @@ public class DataBoardDAO {
     	}
     	return list;
     }
+    /*
+     *                   요청 처리와 관련된 자바 파일을 모아서 => Model 
+     *                   Model => Model + VO + DAO
+     *    JSP =========> Model(Java) ========> DAO(사이트에 필요한 데이터 저장)
+     *     요청 (request)    |
+     *                    1.요청정보 받기 
+     *                    2.DAO연결 
+     *                    3.결과값을 request담아 준다
+     *                      request.setAttribute() => 여러개 사용이 가능  
+     *                      ---------------------
+     *                       화면에 출력해야되는 모든 데이터 전송 
+     */
+    // 총페이지 가지고 오기 
+    public int databoardTotalPage()
+    {
+    	int total=0;
+    	try
+    	{
+    		//1. 미리 생성된 Connection의 주소 얻기 
+    		getConnection();
+    		//2. SQL문장 
+    		String sql="SELECT CEIL(COUNT(*)/10.0) FROM databoard";
+    		//3. 오라클로 전송 (DAO만이 => 오라클)
+    		ps=conn.prepareStatement(sql);
+    		//4. 실행요청을 요청 => 결과값 읽기
+    		ResultSet rs=ps.executeQuery();
+    		//5. rs에 있는 데이터를 total에 저장 => 필요시마다 사용이 가능 
+    		rs.next();
+    		total=rs.getInt(1);
+    		rs.close();
+    	}catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    	}
+    	finally
+    	{
+    		// 반환 => POOL안에 다시 재사용이 가능하게 만든다 
+    		disConnection();
+    	}
+    	return total;
+    }
+    // 새글 올리기 => 파일 업로드 
+    public void databoardInsert(DataBoardVO vo)
+    {
+    	try
+    	{
+    		getConnection();
+    		String sql="INSERT INTO databoard(no,name,subject,content,"
+    				  +"pwd,group_id,filename,filesize) "
+    				  +"VALUES(db_no_seq.nextval,?,?,?,?,"
+    				  +"(SELECT NVL(MAX(group_id)+1,1) FROM databoard),"
+    				  +"?,?)";
+    		// sql문장 전송 
+    		ps=conn.prepareStatement(sql);
+    		// ?에 값을 채운다 
+    		ps.setString(1, vo.getName());
+    		ps.setString(2, vo.getSubject());
+    		ps.setString(3, vo.getContent());
+    		ps.setString(4, vo.getPwd());
+    		ps.setString(5, vo.getFilename());
+    		ps.setInt(6, vo.getFilesize());
+    		
+    		// 실행 명령
+    		ps.executeUpdate(); // Commit포함 
+    	}catch(Exception ex) 
+    	{
+    		ex.printStackTrace();
+    	}
+    	finally
+    	{
+    		disConnection();
+    	}
+    }
+    // 상세보기 => 파일 다운로드 (메소드 => SQL문장이 1개 아니다)
+    // 답변 => SQL문장 5개 , 삭제 => SQL문장 5개 => 일괄 처리 (트랜잭션)
+    /*
+     *   DELETE , UPADTE , INSERT => COMMIT , ROLLBACK
+     *   ** 자바의 JDBC 단점 : AutoCommit을 기본으로 수행 
+     */
+    public DataBoardVO databoardDetailData(int no)
+    {
+    	DataBoardVO vo=new DataBoardVO();
+    	try
+    	{
+    		//1. 조회수 증가
+    		getConnection();
+    		String sql="UPDATE databoard SET "
+    				  +"hit=hit+1 "
+    				  +"WHERE no=?";
+    		ps=conn.prepareStatement(sql);
+    		ps.setInt(1, no);
+    		// 실행 
+    		ps.executeUpdate(); // commit()가 포함 
+    		//2. 상세볼 게시물 가지고 오기 
+    		
+    		sql="SELECT no,name,subject,content,regdate,hit,"
+    		   +"filename,filesize "
+    		   +"FROM databoard "
+    		   +"WHERE no=?";
+    		ps=conn.prepareStatement(sql);
+    		ps.setInt(1, no);
+    		ResultSet rs=ps.executeQuery();
+    		rs.next();
+    		vo.setNo(rs.getInt(1));
+    		vo.setName(rs.getString(2));
+    		vo.setSubject(rs.getString(3));
+    		vo.setContent(rs.getString(4));
+    		vo.setRegdate(rs.getDate(5));
+    		vo.setHit(rs.getInt(6));
+    		vo.setFilename(rs.getString(7));
+    		vo.setFilesize(rs.getInt(8));
+    		rs.close();
+    		/*
+    		 *    핵심 => 페이지 (인라인뷰) => 블록단위로 페이지 설정 
+    		 *    - JOIN / SubQuery 
+    		 *    - INSERT / UPDATE / DELETE 
+    		 *    - Spring : PL/SQL => 모든 상세보기에 댓글이 가능 
+    		 *      ------- DAO(MyBatis)
+    		 *                  -------- Annotation / XML 
+    		 */
+    	}catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    	}
+    	finally
+    	{
+    		disConnection();
+    	}
+    	return vo;
+    }
+    // 답변 / 수정 / 삭제 
 }
 
 
--- DataBoardProject/src/main/java/com/sist/model/DataBoardModel.java
+++ DataBoardProject/src/main/java/com/sist/model/DataBoardModel.java
@@ -1,7 +1,18 @@
 package com.sist.model;
 import java.util.*;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.sist.dao.*;
 import javax.servlet.http.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URLEncoder;
+import java.text.*;
+// 조립기 => 모델
 public class DataBoardModel {
     public void databoardListData(HttpServletRequest request)
     {
@@ -15,5 +26,129 @@ public class DataBoardModel {
     	List<DataBoardVO> list=dao.databoardListData(curpage);
     	//3. JSP로 전송 
     	request.setAttribute("list", list);// 데이터를 추가 재전송 
+    	// 데이터를 보내는 제한이 없다 
+    	String today=new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    	request.setAttribute("today", today);
+    	/*
+    	 *   Date date=new Date();
+    	 *   SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+    	 *   String today=sdf.format(date);
+    	 *   // date,sdf를 활용 (다시 사용)
+    	 */
+    	int totalpage=dao.databoardTotalPage();
+    	request.setAttribute("curpage", curpage);
+    	request.setAttribute("totalpage", totalpage);
+    	// list.jsp => 출력 (list,today,curpage,totalpage) => 화면 출력 명령
+    	
     }
+    // 파일 업로드 / 글쓰기 
+    public void databoardInsert(HttpServletRequest request,
+    		HttpServletResponse response)
+    {
+    	try
+    	{
+    		// 자바 => 90%정도가 라이브러리 (찾기) , 10% 사용자 정의 
+    		// cos , commons-file-upload.jar (Apache)
+    		request.setCharacterEncoding("UTF-8");
+    		//1. 파일업로드 
+    		int maxSize=1024*1024*100; //100MB
+    		String path="c:\\download";
+    		String enctype="UTF-8";// 한글 파일을 사용 
+    		// 라이브러리 => 메소드 (리턴형 , 매개변수)
+    		// static 메소드 (클래스명.메소드), instance메소드 (new를 이용해서)
+    		// Math.random() , String.valueOf()
+    		MultipartRequest mr=
+    				new MultipartRequest(request, 
+    						path,maxSize,enctype,
+    						new DefaultFileRenamePolicy());
+    		// DefaultFileRenamePolicy()=> 파일명이 중복시에 다른 파일명으로 변경 
+    		// a.png => a1.png => a2.png...
+    		//2. 데이터베이스에 첨부 
+    		String name=mr.getParameter("name");
+    		String subject=mr.getParameter("subject");
+    		String content=mr.getParameter("content");
+    		String pwd=mr.getParameter("pwd");
+    		String filename=mr.getOriginalFileName("upload");
+    		
+    		DataBoardVO vo=new DataBoardVO();
+    		vo.setName(name);
+    		vo.setSubject(subject);
+    		vo.setContent(content);
+    		vo.setPwd(pwd);
+    		
+    		// 파일 관련 
+    		if(filename==null) // 업로드가 안된 상태 
+    		{
+    			vo.setFilename("");
+    			vo.setFilesize(0);
+    		}
+    		else
+    		{
+    			File file=new File(path+"\\"+filename);
+    			// 파일 정보 읽기 
+    			vo.setFilename(file.getName());// 파일명만 저장 
+    			vo.setFilesize((int)file.length());
+    			// long length()
+    		}
+    		//2-1 => DAO에 전송 
+    		DataBoardDAO dao=new DataBoardDAO();
+    		dao.databoardInsert(vo);
+    		//3. 이동 => 목록 
+    		response.sendRedirect("list.jsp");
+    	}catch(Exception ex){}
+    }
+    // 상세보기 처리 
+    public void databoardDetailData(HttpServletRequest request)
+    {
+    	// 1. 사용자가 보내준 값을 받는다 
+    	String no=request.getParameter("no");
+    	String page=request.getParameter("page");
+    	// *** 처리는 자바(model)에서 => 화면 출력은 JSP (view) => MV
+    	// 자바에 출력할 데이터를 JSP로 보내주면 JSP에서는 화면만 출력 
+    	// DAO에서 상세볼 데이터를 가지고 온다 
+    	DataBoardDAO dao=new DataBoardDAO();
+    	DataBoardVO vo=dao.databoardDetailData(Integer.parseInt(no));
+    	
+    	// JSP로 출력하기 위해서 데이터를 보내준다 
+    	request.setAttribute("vo", vo);
+    	request.setAttribute("page", page);
+    }
+    // 다운로드 
+    public void databoardDownload(HttpServletRequest request,
+    		HttpServletResponse response)
+    {
+    	try
+    	{
+    		request.setCharacterEncoding("UTF-8");
+    		//1. File명을 받는다 
+    		String fn=request.getParameter("fn");
+    		//2. header 생성 => 실제 데이터전에 전송 (클라이언트로)
+    		//2-1. 파일 크기 , 파일 이름 => 파일다운로드 창을 보여준다
+    		File file=new File("c:\\download\\"+fn);
+    		response.setContentLength((int)file.length());
+    		response.setHeader("Content-Disposition", "attachment;filename="
+    				          +URLEncoder.encode(fn, "UTF-8"));
+    		//////// Header
+    		BufferedInputStream bis=
+    				new BufferedInputStream(new FileInputStream(file));
+    		BufferedOutputStream bos=
+    				new BufferedOutputStream(response.getOutputStream());
+    		
+    		int i=0;
+    		byte[] buffer=new byte[1024];
+    		while((i=bis.read(buffer, 0, 1024))!=-1)
+    		{
+    			bos.write(buffer, 0, i);
+    		}
+    		bis.close();
+    		bos.close();
+    		
+    	}catch(Exception ex){}
+    }
+    
+    
 }
+
+
+
+
